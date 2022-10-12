0cd58c1f6bace5f2e33daf68bcaa8c9ec914e0f8
Tymofii Reizin <timreizin@gmail.com>
2022-10-12 21:13:23 +0200

    Create FloydWarshall.cpp

---
+++ Graphs/Algorithms/FloydWarshall.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+const long long INF = 1e18;
+
+std::vector<std::vector<long long>> floydWarshall(const std::vector<std::vector<long long>> &adj)
+{
+    std::vector<std::vector<long long>> distances = adj;
+    for (int k = 0; k < adj.size(); ++k)
+        for (int i = 0; i < adj.size(); ++i)
+            for (int j = 0; j < adj.size(); ++j)
+                if (distances[i][k] < INF && distances[j][k] < INF)
+                    distances[i][j] = std::min(distances[i][j], distances[i][k] + distances[k][j]);
+    return distances;
+}
