fa71fe8ffe8108b5ca0a5efdd3ef01c0865ec8d1
Tymofii Reizin <timreizin@gmail.com>
2022-10-12 21:16:27 +0200

    Create TopologicalSort.cpp

---
+++ Graphs/Algorithms/TopologicalSort.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+
+const long long INF = 1e18;
+
+std::vector<int> topSort(const std::vector<std::vector<int>> &adj)
+{
+    std::vector<int> sorted;
+    std::vector<int> used(adj.size());
+    
+    auto dfs = [&](auto self, int v) -> bool
+    {
+        used[v] = 1;
+        for (int u : adj[v])
+        {
+            if (used[u] == 2) continue;
+            if (used[u] == 1) return false;
+            bool ret = self(self, v);
+            if (!ret) return false;
+        }
+        sorted.push_back(v);
+        used[v] = 2;
+        return true;
+    };
+    
+    bool res = true;
+    for (int i = 0; i < adj.size(); ++i)
+        if (used[i] == 0)
+            res = res && dfs(dfs, i);
+    if (!res) sorted.clear();
+    reverse(sorted.begin(), sorted.end());
+    return sorted;
+}
