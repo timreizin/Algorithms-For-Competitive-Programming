3a226f47387fcb3597df226c9b60771ce0cb0206
Tymofii Reizin <timreizin@gmail.com>
2022-10-12 19:04:13 +0200

    Create prefixFunction.cpp

---
+++ String Algorithms/prefixFunction.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <string>
+
+std::vector<int> prefixFunction(const std::string &s)
+{
+    std::vector<int> prefixF(s.size());
+    for (int i = 1, j; i < s.size(); ++i)
+    {
+        for (j = prefixF[i - 1]; j > 0 && s[i] != s[j]; j = prefixF[j - 1]) {}
+        prefixF[i] = j;
+        if (s[i] == s[j]) ++prefixF[i];
+    }
+    return prefixF;
+}
