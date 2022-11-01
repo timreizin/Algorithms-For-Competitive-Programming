d6496b9690706130f97b442b2d2896e97cc2f29c
Tymofii Reizin <timreizin@gmail.com>
2022-11-01 11:22:36 +0100

    Create Floyd.cpp

---
+++ Graphs/Algorithms/Floyd.cpp
@@ -0,0 +1,23 @@
+pair<int, int> floyd(int s, vector<int> &next)
+{
+    int a, b;
+    a = b = s;
+    while (a != b)
+    {
+        a = next[a];
+        b = next[next[b]];
+    }
+    a = s;
+    while (a != b)
+    {
+        a = next[a];
+        b = next[b];
+    }
+    int len = 0;
+    do
+    {
+        ++len;
+        b = next[b];
+    } while (b != a);
+    return {a, len};
+}
