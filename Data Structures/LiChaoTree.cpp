348a1d3df82c44706dd79f97b9badcdc9e80cbfa
Tymofii Reizin <timreizin@gmail.com>
2022-11-01 11:30:41 +0100

    Update LiChaoTree.cpp

--- Data Structures/LiChaoTree.cpp
+++ Data Structures/LiChaoTree.cpp
@@ -1,19 +1,3 @@
-template <class T = ll>
-struct Line
-{
-    using type = T;
-    
-    T k, b;
-    
-    Line(T k = 0, T b = LONGINF) : k(k), b(b)
-    {}
-    
-    T operator()(T x)
-    {
-        return k * x + b;
-    }
-};
-
 template <class Function>
 class LiChao
 {
