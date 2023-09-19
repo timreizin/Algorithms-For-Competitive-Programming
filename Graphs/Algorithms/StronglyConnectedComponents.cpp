27e633b7e9fc6886373d3e11f38ad023a93c0fa1
Tymofii Reizin <46033117+timreizin@users.noreply.github.com>
2023-09-19 21:26:10 +0200

    Update StronglyConnectedComponents.cpp

--- Graphs/Algorithms/StronglyConnectedComponents.cpp
+++ Graphs/Algorithms/StronglyConnectedComponents.cpp
@@ -1,11 +1,9 @@
-#include <vector>
-
 std::vector<int> SCC(const std::vector<std::vector<int>> &adj)
 {
     std::vector<int> sorted;
     std::vector<bool> used(adj.size());
-    
-    auto dfsSort = [&](auto self, int v) -> void
+
+    auto dfsSort = [&used, &sorted, &adj](auto self, int v) -> void
     {
         used[v] = true;
         for (int u : adj[v])
@@ -13,25 +11,28 @@ std::vector<int> SCC(const std::vector<std::vector<int>> &adj)
                 self(self, u);
         sorted.push_back(v);
     };
-    
+
     for (int i = 0; i < adj.size(); ++i)
         if (!used[i])
             dfsSort(dfsSort, i);
-    reverse(sorted.begin(), sorted.end());
+
+    std::reverse(sorted.begin(), sorted.end());
     std::vector<std::vector<int>> adjT(adj.size());
     for (int v = 0; v < adj.size(); ++v)
         for (int u : adj[v])
             adjT[u].push_back(v);
     used = std::vector<bool>(adj.size());
     std::vector<int> component(adj.size());
-    
-    auto dfs = [&](auto self, int v, int comp) -> void
+
+    auto dfs = [&used, &component, &adj](auto self, int v, int comp) -> void
     {
         used[v] = true;
         component[v] = comp;
-        for (int u : adj[v]) if (!used[u]) self(self, u, comp);
+        for (int u : adj[v])
+            if (!used[u])
+                self(self, u, comp);
     };
-    
+
     int counter = 0;
     for (int v : sorted)
         if (!used[v])
