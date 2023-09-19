33c040e34febbbb10f17f6c49b89f700bbbc086e
Tymofii Reizin <46033117+timreizin@users.noreply.github.com>
2023-09-19 20:22:27 +0200

    Update 2SAT.cpp

--- Graphs/Algorithms/2SAT.cpp
+++ Graphs/Algorithms/2SAT.cpp
@@ -1,26 +1,36 @@
-void dfs2SAT1(int v, vector<int> &order, vector<bool> &used, vector<vector<int>> &adj)
+void dfs2SAT1(int v, std::vector<int> &order, std::vector<bool> &used, const std::vector<std::vector<int>> &adj)
 {
     used[v] = true;
-    for (int u : adj[v]) if (!used[u]) dfs2SAT1(u, order, used, adj);
+    for (int u : adj[v])
+        if (!used[u])
+            dfs2SAT1(u, order, used, adj);
     order.push_back(v);
 }
 
-void dfs2SAT2(int v, int t, vector<vector<int>> &adj, vector<int> &comp)
+void dfs2SAT2(int v, int t, const std::vector<std::vector<int>> &adj, std::vector<int> &comp)
 {
     comp[v] = t;
-    for (int u : adj[v]) if (comp[u] == 0) dfs2SAT2(u, t, adj, comp);
+    for (int u : adj[v])
+        if (comp[u] == 0)
+            dfs2SAT2(u, t, adj, comp);
 }
 
-bool SAT2(vector<vector<int>> &adj, vector<bool> &result)
+bool SAT2(const std::vector<std::vector<int>> &adj, std::vector<bool> &result)
 {
-    vector<int> order;
-    vector<bool> used(adj.size());
-    for (int i = 0; i < adj.size(); ++i) if (!used[i]) dfs2SAT1(i, order, used, adj);
+    std::vector<int> order;
+    std::vector<bool> used(adj.size());
+    for (int i = 0; i < adj.size(); ++i)
+        if (!used[i])
+            dfs2SAT1(i, order, used, adj);
     reverse(order.begin(), order.end());
-    vector<int> comp(adj.size());
-    vector<vector<int>> adjT(adj.size());
-    for (int i = 0; i < adj.size(); ++i) for (int u : adj[i]) adjT[u].push_back(i);
-    for (int i = 0, j = 1; i < order.size(); ++i) if (comp[order[i]] == 0) dfs2SAT2(order[i], j++, adjT, comp);
+    std::vector<int> comp(adj.size());
+    std::vector<std::vector<int>> adjT(adj.size());
+    for (int i = 0; i < adj.size(); ++i)
+        for (int u : adj[i])
+            adjT[u].push_back(i);
+    for (int i = 0, j = 1; i < order.size(); ++i)
+        if (comp[order[i]] == 0)
+            dfs2SAT2(order[i], j++, adjT, comp);
     for (int i = 0; i < adj.size(); i += 2)
     {
         if (comp[i] == comp[i + 1])
