67b720ebb08f55adb564ff01401d4daa14918c8e
Tymofii Reizin <46033117+timreizin@users.noreply.github.com>
2023-09-19 20:50:02 +0200

    Update KuhnMinWeight.cpp

--- Graphs/Algorithms/KuhnMinWeight.cpp
+++ Graphs/Algorithms/KuhnMinWeight.cpp
@@ -1,33 +1,34 @@
-bool dfsKuhn(int v, vector<bool>& used, vector<int>& matching, vector<vector<int>>& adj)
+int kuhnMinWeight(const std::vector<std::vector<int>> &adj, std::vector<int> &matching, std::vector<std::pair<int, int>> part1)
 {
-    if (used[v]) return false;
-    used[v] = true;
-    for (int u : adj[v])
+    matching = std::vector<int>(adj.size(), -1);
+    std::sort(part1.begin(), part1.end());
+    std::vector<bool> used(adj.size());
+    auto dfs = [&used, &matching, &adj](auto self, int v) -> bool
     {
-        if (matching[u] == -1 || dfsKuhn(matching[u], used, matching, adj))
+        if (used[v])
+            return false;
+        used[v] = true;
+        for (int u : adj[v])
         {
-            matching[u] = v;
-            matching[v] = u;
-            return true;
+            if (matching[u] == -1 || self(self, matching[u]))
+            {
+                matching[u] = v;
+                matching[v] = u;
+                return true;
+            }
         }
-    }
-    return false;
-}
-
-int kuhnMinWeight(vector<vector<int>> &adj, vector<int> &matching, vector<pair<int, int>> part1)
-{
-    matching = vector<int>(adj.size(), -1);
-    sort(part1.begin(), part1.end());
-    vector<bool> used(adj.size());
+        return false;
+    };
     for (bool isFull = true; isFull;)
     {
         isFull = false;
-        used = vector<bool>(adj.size(), false);
+        used = std::vector<bool>(adj.size(), false);
         for (auto [w, v] : part1)
-            if (matching[v] == -1 && dfsKuhn(v, used, matching, adj))
+            if (matching[v] == -1 && dfs(dfs, v))
                 isFull = true;
     }
     int counter = 0;
-    for (auto [w, v] : part1) counter += (matching[v] != -1);
+    for (auto [w, v] : part1)
+        counter += (matching[v] != -1);
     return counter;
 }
