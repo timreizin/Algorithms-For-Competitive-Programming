17bea3a7e0d5a40def8840ddd55b207d5cae4f19
Tymofii Reizin <46033117+timreizin@users.noreply.github.com>
2023-09-19 22:11:27 +0200

    Update Sack.cpp

--- Graphs/Trees/Data Structures/Sack.cpp
+++ Graphs/Trees/Data Structures/Sack.cpp
@@ -1,13 +1,13 @@
 class Sack
 {
 private:
-    
+
     struct Answer
     {
         int maxim = 0;
         int sum = 0;
-        vector<int> counter;
-        
+        std::vector<int> counter;
+
         void update(int v, int x, bool del = false)
         {
             counter[v] += x;
@@ -18,39 +18,42 @@ private:
                     sum = 0;
                     maxim = counter[v];
                 }
-                if (counter[v] == maxim) sum += v;
+                if (counter[v] == maxim)
+                    sum += v;
             }
         }
-        
+
         int operator()()
         {
             return sum;
         }
-        
+
         void clear()
         {
             sum = 0;
             maxim = 0;
         }
     };
-    
-    vector<vector<int>> adj;
-    vector<int> tin, tout, tTov;
-    vector<int> values;
-    vector<int> result;
-    vector<int> sizes;
+
+    std::vector<std::vector<int>> adj;
+    std::vector<int> tin, tout, tTov;
+    std::vector<int> values;
+    std::vector<int> result;
+    std::vector<int> sizes;
     Answer answer;
-    
+
     int calculate(int v, int p, int &t)
     {
         tTov.push_back(v);
         tin[v] = t++;
         sizes[v] = 1;
-        for (int u : adj[v]) if (u != p) sizes[v] += calculate(u, v, t);
+        for (int u : adj[v])
+            if (u != p)
+                sizes[v] += calculate(u, v, t);
         tout[v] = t - 1;
         return sizes[v];
     }
-    
+
     int findHeavy(int v, int p)
     {
         int maxim = 0, heavy = -1;
@@ -64,12 +67,15 @@ private:
         }
         return heavy;
     }
-    
+
     void dfs(int v, int p, bool isHeavy = false)
     {
         int heavy = findHeavy(v, p);
-        for (int u : adj[v]) if (u != p && u != heavy) dfs(u, v);
-        if (heavy != -1) dfs(heavy, v, 1);
+        for (int u : adj[v])
+            if (u != p && u != heavy)
+                dfs(u, v);
+        if (heavy != -1)
+            dfs(heavy, v, 1);
         for (int i = tin[v]; i <= tout[v]; ++i)
         {
             if (i == tin[heavy])
@@ -82,14 +88,15 @@ private:
         result[v] = answer();
         if (!isHeavy)
         {
-            for (int i = tin[v]; i <= tout[v]; ++i) answer.update(values[tTov[i]], -1, 1);
+            for (int i = tin[v]; i <= tout[v]; ++i)
+                answer.update(values[tTov[i]], -1, 1);
             answer.clear();
         }
     }
-    
+
 public:
-    
-    Sack(vector<vector<int>> adj, vector<int> values) : adj(adj), values(values)
+
+    Sack(std::vector<std::vector<int>> adj, std::vector<int> values) : adj(adj), values(values)
     {
         result.resize(adj.size());
         sizes.resize(adj.size());
@@ -101,10 +108,10 @@ public:
         calculate(1, 0, t);
         dfs(1, 0);
     }
-    
-    vector<int> operator()()
+
+    std::vector<int> operator()()
     {
         return result;
     }
-    
+
 };
