f9e2b023b750cf898fea7292694391a9bd5a6203
Tymofii Reizin <46033117+timreizin@users.noreply.github.com>
2023-09-19 22:07:55 +0200

    Update HeavyLightDecomposition.cpp

--- Graphs/Trees/Data Structures/HeavyLightDecomposition.cpp
+++ Graphs/Trees/Data Structures/HeavyLightDecomposition.cpp
@@ -1,17 +1,17 @@
-template <class Structure, ll zero = 0>
+template <class T, class Structure>
 class HeavyLightDecomposition
 {
 private:
-    
+
     int n;
-    
+
     Structure structure;
-    vector<int> depth;
-    vector<int> head;
-    vector<int> pos;
-    vector<int> parent;
-    
-    int findHeavies(int v, vector<vector<int>> &adj)
+    std::vector<int> depth;
+    std::vector<int> head;
+    std::vector<int> pos;
+    std::vector<int> parent;
+
+    int findHeavies(int v, std::vector<std::vector<int>> &adj)
     {
         int size = 1, maxSize = 0;
         for (int &u : adj[v])
@@ -25,24 +25,27 @@ private:
                 if (childSize > maxSize)
                 {
                     maxSize = childSize;
-                    swap(adj[v].front(), u);
+                    std::swap(adj[v].front(), u);
                 }
             }
         }
         return size;
     }
-    
-    void decompose(int v, int h, int &curPos, vector<vector<int>> &adj)
+
+    void decompose(int v, int h, int &curPos, std::vector<std::vector<int>> &adj)
     {
         head[v] = h;
         pos[v] = curPos++;
-        if (!adj[v].empty() && adj[v].front() != parent[v]) decompose(adj[v].front(), h, curPos, adj);
-        for (int u : adj[v]) if (u != adj[v].front() && u != parent[v]) decompose(u, u, curPos, adj);
+        if (!adj[v].empty() && adj[v].front() != parent[v])
+            decompose(adj[v].front(), h, curPos, adj);
+        for (int u : adj[v])
+            if (u != adj[v].front() && u != parent[v])
+                decompose(u, u, curPos, adj);
     }
-    
+
 public:
-    
-    HeavyLightDecomposition(vector<vector<int>> adj, vector<ll> a = vector<ll>()) : n((int)adj.size())
+
+    HeavyLightDecomposition(std::vector<std::vector<int>> adj, std::vector<T> a = std::vector<T>()) : n((int)adj.size())
     {
         depth.resize(n);
         head.resize(n);
@@ -51,26 +54,28 @@ public:
         findHeavies(1, adj);
         int curPos = 0;
         decompose(1, 1, curPos, adj);
-        if (a.empty()) a.resize(n - 1, zero);
-        vector<ll> help(n - 1);
-        for (int i = 0; i < n - 1; ++i) help[pos[i + 1]] = a[i];
+        if (a.empty()) a.resize(n - 1, T(0));
+        std::vector<T> help(n - 1);
+        for (int i = 0; i < n - 1; ++i)
+            help[pos[i + 1]] = a[i];
         structure = Structure(help);
     }
-    
-    ll get(int a, int b)
+
+    T get(int a, int b)
     {
-        ll res = 0;
+        T res = 0;
         for (; head[a] != head[b]; b = parent[head[b]])
         {
-            if (depth[head[a]] > depth[head[b]]) swap(a, b);
+            if (depth[head[a]] > depth[head[b]])
+                std::swap(a, b);
             res += structure.get(pos[head[b]], pos[b]);
         }
-        return res + structure.get(min(pos[a], pos[b]), max(pos[a], pos[b]));
+        return res + structure.get(std::min(pos[a], pos[b]), std::max(pos[a], pos[b]));
     }
-    
-    void update(int a, ll x)
+
+    void update(int a, T x)
     {
         structure.update(pos[a], pos[a], x);
     }
-    
+
 };
