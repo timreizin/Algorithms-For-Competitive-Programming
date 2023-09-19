ebfe70319618323e1a9cc414f35265c6edd65471
Tymofii Reizin <46033117+timreizin@users.noreply.github.com>
2023-09-19 22:35:06 +0200

    Update Mo's Algorithm.cpp

--- Data Structures/Mo's Algorithm.cpp
+++ Data Structures/Mo's Algorithm.cpp
@@ -8,35 +8,37 @@ private:
         int r;
         int index;
 
-        query(int al, int ar, int aindex) : l(al), r(ar), index(aindex)
+        query(int l, int r, int index) : l(l), r(r), index(index)
         {}
     };
 
     struct value
     {
-        vector<int> counter;
+        std::vector<int> counter;
         int res = 0;
     };
 
     int size;
     int divider;
     int n;
-    vector<int> arr;
-    vector<vector<query>> blocks;
+    std::vector<int> arr;
+    std::vector<std::vector<query>> blocks;
 
     void add(int ind, value &val)
     {
         ++val.counter[arr[ind]];
-        if (val.counter[arr[ind]] == 1) ++val.res;
+        if (val.counter[arr[ind]] == 1)
+            ++val.res;
     }
 
     void del(int ind, value &val)
     {
         --val.counter[arr[ind]];
-        if (val.counter[arr[ind]] == 0) --val.res;
+        if (val.counter[arr[ind]] == 0)
+            --val.res;
     }
 
-    void adjust(int &l, int &r, query &q, value &val)
+    void adjust(int &l, int &r, const query &q, value &val)
     {
         while (r < q.r)
         {
@@ -57,20 +59,24 @@ private:
 
 public:
 
-    Mo(vector<int> aarr, vector<pair<int, int>> queries, int an = -1, int adivider = -1) : size((int)queries.size()), arr(aarr)
+    Mo(const std::vector<int> arr, const std::vector<std::pair<int, int>> queries, int n = -1, int divider = -1) : size((int)queries.size()), arr(arr), divider(divider), n(n)
     {
-        n = an;
-        if (n == -1) for (auto [l, r] : queries) n = max(n, r);
-        if (adivider == -1) adivider = sqrt(n);
-        divider = max(adivider, 1);
+        if (n == -1)
+            for (auto [l, r] : queries)
+                n = std::max(n, r);
+        if (divider == -1)
+            divider = sqrt(n);
+        divider = std::max(divider, 1);
         blocks.resize(n / divider + 1);
-        for (int i = 0; i < queries.size(); ++i) blocks[queries[i].first / divider].emplace_back(queries[i].first, queries[i].second, i);
-        for (auto &i : blocks) sort(i.begin(), i.end(), [](query a, query b){ return a.r < b.r; });
+        for (int i = 0; i < queries.size(); ++i)
+            blocks[queries[i].first / divider].emplace_back(queries[i].first, queries[i].second, i);
+        for (auto &i : blocks)
+            std::sort(i.begin(), i.end(), [](query a, query b){ return a.r < b.r; });
     }
 
-    vector<int> operator()()
+    std::vector<int> operator()()
     {
-        vector<int> res(size);
+        std::vector<int> res(size);
         for (int i = 0; i < blocks.size(); ++i)
         {
             int l = i * divider, r = i * divider - 1;
