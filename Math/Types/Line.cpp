e1908647027c37e7d6f3934f1b944f641d68c0ef
Tymofii Reizin <46033117+timreizin@users.noreply.github.com>
2023-09-19 19:31:52 +0200

    Update Line.cpp

--- Math/Types/Line.cpp
+++ Math/Types/Line.cpp
@@ -1,13 +1,13 @@
-template <class T = ll>
+template <class T = long long>
 struct Line
 {
     using type = T;
-    
+
     T k, b;
-    
-    Line(T k = 0, T b = LONGINF) : k(k), b(b)
+
+    Line(T k = 0, T b = 0) : k(k), b(b)
     {}
-    
+
     T operator()(T x)
     {
         return k * x + b;
