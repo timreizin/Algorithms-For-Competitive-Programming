f3da22c8bd2bc6470f522d58986a138af39d071f
Tymofii Reizin <46033117+timreizin@users.noreply.github.com>
2023-09-19 21:20:19 +0200

    Update MSTPrim.cpp

--- Graphs/Algorithms/MSTPrim.cpp
+++ Graphs/Algorithms/MSTPrim.cpp
@@ -1,18 +1,24 @@
-ll prim(vector<vector<pair<int, ll>>>& adj, vector<pair<pair<int, int>, ll>>& mst)
+long long prim(std::vector<std::vector<std::pair<int, long long>>> &adj, std::vector<std::pair<std::pair<int, int>, long long>> &mst)
 {
+    constexpr long long INF = 1e18;
     int n = (int)adj.size() - 1;
-    vector<bool> used(n + 1);
-    vector<pair<ll, int>> minEdges(n + 1, {INF, -1});
+    std::vector<bool> used(n + 1);
+    std::vector<std::pair<long long, int>> minEdges(n + 1, {INF, -1});
     minEdges[1].first = 0;
-    ll minCost = 0;
+    long long minCost = 0;
     for (int i = 0; i < n; ++i)
     {
         int v = -1;
-        for (int j = 1; j <= n; ++j) if (!used[j] && (v == -1 || minEdges[j].first < minEdges[v].first)) v = j;
+        for (int j = 1; j <= n; ++j) 
+            if (!used[j] && (v == -1 || minEdges[j].first < minEdges[v].first))
+                v = j;
         used[v] = true;
         minCost += minEdges[v].first;
-        if (minEdges[v].second != -1) mst.emplace_back(make_pair(v, minEdges[v].second), minEdges[v].first);
-        for (auto [u, w] : adj[v]) if (w < minEdges[u].first) minEdges[u] = {w, v};
+        if (minEdges[v].second != -1)
+            mst.emplace_back(std::pair(v, minEdges[v].second), minEdges[v].first);
+        for (auto [u, w] : adj[v])
+            if (w < minEdges[u].first)
+                minEdges[u] = {w, v};
     }
     return minCost;
 }
