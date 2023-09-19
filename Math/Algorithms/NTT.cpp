cb0c51dad25fe340c43c6ca7cbc103eda8bf2af4
Tymofii Reizin <46033117+timreizin@users.noreply.github.com>
2023-09-19 19:21:02 +0200

    Update NTT.cpp

--- Math/Algorithms/NTT.cpp
+++ Math/Algorithms/NTT.cpp
@@ -1,14 +1,16 @@
-void ntt(vector<ll> &a, vector<int> &indexInEnd, vector<ll> &roots)
+void ntt(std::vector<long long> &a, const std::vector<int> &indexInEnd, const std::vector<long long> &roots)
 {
     int n = (int)a.size();
-    for (int i = 0; i < n; ++i) if (i < indexInEnd[i]) swap(a[i], a[indexInEnd[i]]);
+    for (int i = 0; i < n; ++i)
+        if (i < indexInEnd[i])
+            std::swap(a[i], a[indexInEnd[i]]);
     for (int length = 1, rootStep = n / 2; length < n; length <<= 1, rootStep >>= 1)
     {
         for (int i = 0; i < n; i += 2 * length)
         {
             for (int j = i; j < i + length; ++j)
             {
-                ll value = (roots[(j - i) * rootStep] * a[j + length]) % MOD;
+                long long value = (roots[(j - i) * rootStep] * a[j + length]) % MOD;
                 a[j + length] = (a[j] - value + MOD) % MOD;
                 a[j] = (a[j] + value) % MOD;
             }
@@ -16,35 +18,40 @@ void ntt(vector<ll> &a, vector<int> &indexInEnd, vector<ll> &roots)
     }
 }
 
-void inverseNtt(vector<ll> &a, vector<int> &indexInEnd, vector<ll> &roots)
+void inverseNtt(std::vector<long long> &a, const std::vector<int> &indexInEnd, const std::vector<long long> &roots)
 {
     ntt(a, indexInEnd, roots);
-    ll inverseSize = binPow(a.size(), MOD - 2);
-    for (int i = 0; i < a.size(); ++i) a[i] = (a[i] * inverseSize) % MOD;
+    long long inverseSize = binPow(a.size(), MOD - 2);
+    for (int i = 0; i < a.size(); ++i)
+        a[i] = (a[i] * inverseSize) % MOD;
     reverse(a.begin() + 1, a.end());
 }
 
-vector<ll> convolution(vector<ll> a, vector<ll> b)
+std::vector<long long> convolution(std::vector<long long> a, std::vector<long long> b)
 {
-    if (a.size() < b.size()) a.swap(b);
+    if (a.size() < b.size())
+        a.swap(b);
     int resultSize = (int)a.size() + (int)b.size() - 1;
     int n = 1 << (32 - __builtin_clz(resultSize) - ((resultSize & (resultSize - 1)) == 0));
     a.resize(n);
     b.resize(n);
-    ll g = binPow(3, (MOD - 1) / n);
-    vector<int> indexInEnd(n);
+    long long g = binPow(3, (MOD - 1) / n);
+    std::vector<int> indexInEnd(n);
     for (int i = 1, log2 = -1, length = (31 - __builtin_clz(n)); i < n; ++i)
     {
-        if (!(i & (i - 1))) ++log2;
+        if (!(i & (i - 1)))
+            ++log2;
         indexInEnd[i] = indexInEnd[i ^ (1 << log2)];
         indexInEnd[i] |= (1 << (length - log2 - 1));
     }
-    vector<ll> roots(n);
+    std::vector<long long> roots(n);
     roots[0] = 1;
-    for (int i = 1; i < n; ++i) roots[i] = (roots[i - 1] * g) % MOD;
+    for (int i = 1; i < n; ++i)
+        roots[i] = (roots[i - 1] * g) % MOD;
     ntt(a, indexInEnd, roots);
     ntt(b, indexInEnd, roots);
-    for (int i = 0; i < n; ++i) a[i] = (a[i] * b[i]) % MOD;
+    for (int i = 0; i < n; ++i)
+        a[i] = (a[i] * b[i]) % MOD;
     inverseNtt(a, indexInEnd, roots);
     a.resize(resultSize);
     return a;
