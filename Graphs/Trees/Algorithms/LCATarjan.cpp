919243efebb5d9601a1117271d99fe4b2f261998
Tymofii Reizin <46033117+timreizin@users.noreply.github.com>
2023-09-19 21:54:32 +0200

    Update LCATarjan.cpp

--- Graphs/Trees/Algorithms/LCATarjan.cpp
+++ Graphs/Trees/Algorithms/LCATarjan.cpp
@@ -1,23 +1,24 @@
-void dfsTarjan(int v, vector<bool>& used, vector<vector<int>>& adj, vector<vector<pair<int, int>>>& queries, DSU& dsu)
-{
-    used[v] = true;
-    dsu.makeSet(v, v);
-    for (int u : adj[v])
-    {
-        if (!used[u])
-        {
-            dfsTarjan(u, used, adj, queries, dsu);
-            dsu.uniteSets(v, u);
-            dsu.updateValue(v, v - dsu.getValue(v));
-        }
-    }
-    for (auto& [u, ans] : queries[v]) if (used[u]) ans = dsu.getValue(u);
-}
-
-void lcaTarjan(vector<vector<int>>& adj, vector<vector<pair<int, int>>>& queries)
+void lcaTarjan(const std::vector<std::vector<int>> &adj, std::vector<std::vector<std::pair<int, int>>> &queries)
 {
     int n = (int)adj.size() - 1;
     DSU dsu(n + 1);
-    vector<bool> used(n + 1);
-    dfsTarjan(1, used, adj, queries, dsu);
+    std::vector<bool> used(n + 1);
+    auto dfs = [&used, &adj, &queries, &dsu](auto self, int v) -> void
+    {
+        used[v] = true;
+        dsu.makeSet(v, v);
+        for (int u : adj[v])
+        {
+            if (!used[u])
+            {
+                self(self, u);
+                dsu.uniteSets(v, u);
+                dsu.updateValue(v, v - dsu.getValue(v));
+            }
+        }
+        for (auto& [u, ans] : queries[v])
+            if (used[u])
+                ans = dsu.getValue(u);
+    };
+    dfs(dfs, 1);
 }
