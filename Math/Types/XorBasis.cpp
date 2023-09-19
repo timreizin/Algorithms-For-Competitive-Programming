fa82af2bb3cb3b8db48b21af6bbae44187c1cd37
Tymofii Reizin <46033117+timreizin@users.noreply.github.com>
2023-09-19 19:41:36 +0200

    Update XorBasis.cpp

--- Math/Types/XorBasis.cpp
+++ Math/Types/XorBasis.cpp
@@ -1,22 +1,25 @@
 struct XorBasis
 {
-    vector<ll> basis;
-    
-    void insert(ll x)
+    std::vector<long long> basis;
+
+    void insert(long long x)
     {
-        for (ll i : basis) x = min(x, x ^ i);
-        if (x != 0) basis.push_back(x);
+        for (long long i : basis)
+            x = std::min(x, x ^ i);
+        if (x != 0)
+            basis.push_back(x);
     }
-    
-    bool check(ll x)
+
+    bool check(long long x)
     {
-        for (ll i : basis) x = min(x, x ^ i);
+        for (long long i : basis)
+            x = std::min(x, x ^ i);
         return x == 0;
     }
-    
+
     int size()
     {
         return (int)basis.size();
     }
-    
+
 };
