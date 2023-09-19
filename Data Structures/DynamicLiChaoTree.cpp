500468893e38309d0d5e3f9c1a4d23a1921df28a
Tymofii Reizin <46033117+timreizin@users.noreply.github.com>
2023-09-19 22:16:27 +0200

    Update DynamicLiChaoTree.cpp

--- Data Structures/DynamicLiChaoTree.cpp
+++ Data Structures/DynamicLiChaoTree.cpp
@@ -2,21 +2,23 @@ template <class Function>
 class DynamicLiChao
 {
 private:
-    
+
     using T = typename Function::type;
-    
+
     T minValue, maxValue;
-    vector<Function> tree;
-    vector<pair<int, int>> child;
-    
+    std::vector<Function> tree;
+    std::vector<std::pair<int, int>> child;
+
     void update(T a, T b, T l, T r, int v, Function func)
     {
         if (a > r || b < l) return;
         T m = l + (r - l) / 2;
         if (a == l && b == r)
         {
-            if (func(m) < tree[v](m)) swap(tree[v], func);
-            if (func(l) >= tree[v](l) && func(r) >= tree[v](r)) return;
+            if (func(m) < tree[v](m))
+                swap(tree[v], func);
+            if (func(l) >= tree[v](l) && func(r) >= tree[v](r))
+                return;
             if (func(l) < tree[v](l))
             {
                 if (child[v].first == -1)
@@ -60,43 +62,53 @@ private:
             update(max(a, m + 1), b, m + 1, r, child[v].second, func);
         }
     }
-    
+
     T get(T p, T l, T r, int v)
     {
-        if (v == -1) return LONGINF;
-        if (l == r) return tree[v](p);
+        if (l == r)
+            return tree[v](p);
         T m = l + (r - l) / 2;
-        if (p <= m) return min(tree[v](p), get(p, l, m, child[v].first));
-        else return min(tree[v](p), get(p, m + 1, r, child[v].second));
+        if (p <= m)
+        {
+            if (child[v].first == -1)
+                return tree[v](p);
+            return min(tree[v](p), get(p, l, m, child[v].first));
+        }
+        else
+        {
+            if (child[v].second == -1)
+                return tree[v](p);
+            return min(tree[v](p), get(p, m + 1, r, child[v].second));
+        }
     }
-    
+
 public:
-    
+
     DynamicLiChao(T maxValue) : minValue(0), maxValue(maxValue)
     {
         child.emplace_back(-1, -1);
         tree.push_back(Line<T>());
     }
-    
+
     DynamicLiChao(T minValue = -1e9, T maxValue = 1e9) : minValue(minValue), maxValue(maxValue)
     {
         child.emplace_back(-1, -1);
         tree.push_back(Line<T>());
     }
-    
+
     void update(Function func)
     {
         update(minValue, maxValue, minValue, maxValue, 0, func);
     }
-    
+
     void update(T a, T b, Function func)
     {
         update(a, b, minValue, maxValue, 0, func);
     }
-    
+
     T get(T p)
     {
         return get(p, minValue, maxValue, 0);
     }
-    
+
 };
