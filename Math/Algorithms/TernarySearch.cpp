bf07c3690ecfbf054f951685a3b5cc374c3799c2
Tymofii Reizin <46033117+timreizin@users.noreply.github.com>
2023-09-19 19:24:59 +0200

    Update TernarySearch.cpp

--- Math/Algorithms/TernarySearch.cpp
+++ Math/Algorithms/TernarySearch.cpp
@@ -1,10 +1,14 @@
-double ternarySearch(ld l, ld r, ld (*f)(ld), ld EPS)
+
+template <class T>
+T ternarySearch(T l, T r, const T (*f)(T), const T &EPS)
 {
     while (r - l > EPS)
     {
-        ld m1 = l + (r - l) / 3, m2 = r - (r - l) / 3;
-        if (f(m1) < f(m2)) l = m1;
-        else r = m2;
+        T m1 = l + (r - l) / 3, m2 = r - (r - l) / 3;
+        if (f(m1) < f(m2))
+            l = m1;
+        else
+            r = m2;
     }
     return f(l);
 }
