c2da1aecd1b274bd20a766cc866e34fe96c1554d
Tymofii Reizin <46033117+timreizin@users.noreply.github.com>
2023-09-19 19:37:41 +0200

    Update Matrix.cpp

--- Math/Types/Matrix.cpp
+++ Math/Types/Matrix.cpp
@@ -3,23 +3,21 @@ class Matrix
 {
 private:
 
-    vector<vector<T>> matrix;
+    std::vector<std::vector<T>> matrix;
 
 public:
 
     Matrix() {}
 
-    Matrix(int n, int m, T val = T())
-    {
-        matrix = vector<vector<T>>(n, vector<T>(m, val));
-    }
+    Matrix(int n, int m, T val = T()) : matrix(n, std::vector(m, val))
+    {}
 
-    Matrix(vector<vector<T>> pmatrix) : matrix(pmatrix)
+    Matrix(std::vector<std::vector<T>> matrix) : matrix(matrix)
     {}
 
     void resize(const int newN, const int newM)
     {
-        matrix = vector<vector<T>>(newN, vector<T>(newM, T()));
+        matrix = vector(newN, vector(newM, T()));
     }
 
     void clear()
@@ -27,17 +25,17 @@ public:
         matrix.clear();
     }
 
-    pair<int, int> size() const
+    std::pair<int, int> size() const
     {
         return {matrix.size(), (matrix.size() == 0 ? 0 : matrix[0].size())};
     }
 
-    vector<T>& operator[](const int index)
+    std::vector<T>& operator[](const int index)
     {
         return matrix[index];
     }
 
-    vector<T> operator[](const int index) const
+    std::vector<T> operator[](const int index) const
     {
         return matrix[index];
     }
@@ -45,36 +43,42 @@ public:
     friend Matrix<T> operator*(const Matrix<T> &m, const T &n)
     {
         Matrix<T> result = m;
-        for (vector<T> &i : result.matrix) for (T &j : i) j *= n;
+        for (std::vector<T> &i : result.matrix)
+            for (T &j : i)
+                j *= n;
         return result;
     }
 
     friend Matrix<T> operator*(const T &n, const Matrix<T> &m)
     {
         Matrix<T> result = m;
-        for (vector<T> &i : result.matrix) for (T &j : i) j *= n;
+        for (std::vector<T> &i : result.matrix)
+            for (T &j : i)
+                j *= n;
         return result;
     }
 
-    friend vector<T> operator*(const Matrix<T> &m, const vector<T> &v)
+    friend std::vector<T> operator*(const Matrix<T> &m, const std::vector<T> &v)
     {
-        vector<T> result;
-        for (const vector<T> &i : m.matrix)
+        std::vector<T> result;
+        for (const std::vector<T> &i : m.matrix)
         {
             T sum = 0;
-            for (int j = 0; j < v.size(); ++j) sum += i[j] * v[j];
+            for (int j = 0; j < v.size(); ++j)
+                sum += i[j] * v[j];
             result.push_back(sum);
         }
         return result;
     }
 
-    friend vector<T> operator*(const vector<T> &v, const Matrix<T> &m)
+    friend std::vector<T> operator*(const std::vector<T> &v, const Matrix<T> &m)
     {
-        vector<T> result;
-        for (vector<T> i : m.matrix)
+        std::vector<T> result;
+        for (std::vector<T> i : m.matrix)
         {
             T sum = 0;
-            for (int j = 0; j < v.size(); ++j) sum += i[j] * v[j];
+            for (int j = 0; j < v.size(); ++j)
+                sum += i[j] * v[j];
             result.push_back(sum);
         }
         return result;
@@ -84,15 +88,9 @@ public:
     {
         Matrix<T> result(m1.size().first, m2.size().second);
         for (int i = 0; i < result.size().first; ++i)
-        {
             for (int j = 0; j < result.size().second; ++j)
-            {
                 for (int k = 0; k < m1.size().first; ++k)
-                {
                     result[i][j] += m1[i][k] * m2[k][j];
-                }
-            }
-        }
         return result;
     }
 
@@ -106,17 +104,3 @@ public:
         *this = *this * m;
     }
 };
-
-template <typename T>
-Matrix<T> binPow(Matrix<T> a, ll n)
-{
-    Matrix<T> res(a.size().first, a.size().second);
-    for (int i = 0; i < res.size().first; ++i) res[i][i] = T(1);
-    while (n)
-    {
-        if (n & 1) res *= a;
-        a *= a;
-        n >>= 1;
-    }
-    return res;
-}
