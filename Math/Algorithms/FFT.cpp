ad07ccfdad9500f9d2542306256a2a7bbdcd5410
Tymofii Reizin <46033117+timreizin@users.noreply.github.com>
2023-09-19 19:11:47 +0200

    Update FFT.cpp

--- Math/Algorithms/FFT.cpp
+++ Math/Algorithms/FFT.cpp
@@ -1,14 +1,16 @@
-void fft(vector<complex<ld>> &a, vector<int> &indexInEnd, vector<complex<ld>> &roots)
+void fft(std::vector<std::complex<long double>> &a, std::vector<int> &indexInEnd, std::vector<std::complex<long double>> &roots)
 {
     int n = (int)a.size();
-    for (int i = 0; i < n; ++i) if (i < indexInEnd[i]) swap(a[i], a[indexInEnd[i]]);
+    for (int i = 0; i < n; ++i)
+        if (i < indexInEnd[i])
+            swap(a[i], a[indexInEnd[i]]);
     for (int length = 1, rootStep = n / 2; length < n; length <<= 1, rootStep >>= 1)
     {
         for (int i = 0; i < n; i += 2 * length)
         {
             for (int j = i; j < i + length; ++j)
             {
-                complex<ld> value = roots[(j - i) * rootStep] * a[j + length];
+                std::complex<long double> value = roots[(j - i) * rootStep] * a[j + length];
                 a[j + length] = a[j] - value;
                 a[j] = a[j] + value;
             }
@@ -16,39 +18,45 @@ void fft(vector<complex<ld>> &a, vector<int> &indexInEnd, vector<complex<ld>> &r
     }
 }
 
-void inverseFft(vector<complex<ld>> &a, vector<int> &indexInEnd, vector<complex<ld>> &roots)
+void inverseFft(std::vector<std::complex<long double>> &a, std::vector<int> &indexInEnd, std::vector<std::complex<long double>> &roots)
 {
     fft(a, indexInEnd, roots);
-    for (int i = 0; i < a.size(); ++i) a[i] /= a.size();
+    for (int i = 0; i < a.size(); ++i)
+        a[i] /= a.size();
     reverse(a.begin() + 1, a.end());
 }
 
-vector<ll> convolution(vector<ll> &a, vector<ll> &b)
+std::vector<long long> convolution(std::vector<long long> &a, std::vector<long long> &b)
 {
     int resultSize = (int)a.size() + (int)b.size() - 1;
     int n = 1 << (32 - __builtin_clz(resultSize) - ((resultSize & (resultSize - 1)) == 0));
-    vector<complex<ld>> tA(n), tB(n);
-    for (int i = 0; i < a.size(); ++i) tA[i] = a[i];
-    for (int i = 0; i < b.size(); ++i) tB[i] = b[i];
-    vector<int> indexInEnd(n);
+    std::vector<std::complex<long double>> tA(n), tB(n);
+    for (int i = 0; i < a.size(); ++i)
+        tA[i] = a[i];
+    for (int i = 0; i < b.size(); ++i)
+        tB[i] = b[i];
+    std::vector<int> indexInEnd(n);
     for (int i = 1, log2 = -1, length = (31 - __builtin_clz(n)); i < n; ++i)
     {
-        if (!(i & (i - 1))) ++log2;
+        if (!(i & (i - 1)))
+            ++log2;
         indexInEnd[i] = indexInEnd[i ^ (1 << log2)];
         indexInEnd[i] |= (1 << (length - log2 - 1));
     }
-    vector<complex<ld>> roots(n);
+    std::vector<std::complex<long double>> roots(n);
     for (int i = 0; i < n; ++i)
     {
-        ld alpha = 2 * PI * i / n;
+        long double alpha = 2 * std::numbers::pi * i / n;
         roots[i] = {cos(alpha), sin(alpha)};
     }
     fft(tA, indexInEnd, roots);
     fft(tB, indexInEnd, roots);
-    vector<complex<ld>> tC(n);
-    for (int i = 0; i < n; ++i) tC[i] = tA[i] * tB[i];
+    std::vector<std::complex<long double>> tC(n);
+    for (int i = 0; i < n; ++i)
+        tC[i] = tA[i] * tB[i];
     inverseFft(tC, indexInEnd, roots);
-    vector<ll> c((int)a.size() + (int)b.size() - 1);
-    for (int i = 0; i < (int)c.size(); ++i) c[i] = (long long)(tC[i].real() + 0.5);
+    std::vector<long long> c((int)a.size() + (int)b.size() - 1);
+    for (int i = 0; i < (int)c.size(); ++i)
+        c[i] = (long long)(tC[i].real() + 0.5);
     return c;
 }
