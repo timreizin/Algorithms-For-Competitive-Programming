0d65428fcfa756b66618de9ebaf0fc3622a796c7
Tymofii Reizin <46033117+timreizin@users.noreply.github.com>
2023-09-19 21:18:18 +0200

    Update MSTKruskal.cpp

--- Graphs/Algorithms/MSTKruskal.cpp
+++ Graphs/Algorithms/MSTKruskal.cpp
@@ -1,20 +1,24 @@
-ll kruskal(vector<vector<pair<int, ll>>>& adj, vector<pair<pair<int, int>, ll>>& mst)
+long long kruskal(const std::vector<std::vector<std::pair<int, long long>>> &adj, std::vector<std::pair<std::pair<int, int>, long long>> &mst)
 {
-    vector<pair<ll, pair<int, int>>> edges;
+    std::vector<std::pair<long long, std::pair<int, int>>> edges;
     int n = (int)adj.size() - 1;
     DSU dsu(n);
-    for (int i = 1; i <= n; ++i) for (auto [u, w] : adj[i]) edges.emplace_back(w, make_pair(i, u));
-    sort(edges.begin(), edges.end());
-    ll minCost = 0;
+    for (int i = 1; i <= n; ++i)
+        for (auto [u, w] : adj[i])
+            edges.emplace_back(w, std::pair(i, u));
+    std::sort(edges.begin(), edges.end());
+    long long minCost = 0;
     int countEdges = 0;
     for (auto [w, e] : edges)
     {
-        if (dsu.isInOneSet(e.first, e.second)) continue;
+        if (dsu.isInOneSet(e.first, e.second))
+            continue;
         dsu.uniteSets(e.first, e.second);
         minCost += w;
         mst.emplace_back(e, w);
         ++countEdges;
-        if (countEdges == n - 1) break;
+        if (countEdges == n - 1)
+            break;
     }
     return minCost;
 }
