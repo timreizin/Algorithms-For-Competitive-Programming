03c303147cafb967ac8cbbe4fb483cbfaa8904a0
Tymofii Reizin <46033117+timreizin@users.noreply.github.com>
2023-09-19 20:45:12 +0200

    Update Kuhn.cpp

--- Graphs/Algorithms/Kuhn.cpp
+++ Graphs/Algorithms/Kuhn.cpp
@@ -1,25 +1,8 @@
-int kuhn(const std::vector<std::vector<int>> &adj, std::vector<int> &matching, std::vector<int> part1 = std::vector<int>())
+int kuhnMinWeight(const std::vector<std::vector<int>> &adj, std::vector<int> &matching, std::vector<std::pair<int, int>> part1)
 {
     matching = std::vector<int>(adj.size(), -1);
+    std::sort(part1.begin(), part1.end());
     std::vector<bool> used(adj.size());
-    if (part1.empty())
-    {
-        std::vector<int> part2;
-        auto dfs = [&part1, &part2, &adj, &used](auto self, int v, bool part) -> void
-        {
-            used[v] = true;
-            if (part) part1.push_back(v);
-            else part2.push_back(v);
-            for (int u : adj[v])
-                if (!used[u])
-                    self(self, u, !part);
-        };
-        for (int i = 0; i < adj.size(); ++i)
-            if (!used[i])
-                dfs(dfs, i, true);
-        if (part1.size() > part2.size())
-            part1.swap(part2);
-    }
     auto dfs = [&used, &matching, &adj](auto self, int v) -> bool
     {
         if (used[v])
@@ -40,12 +23,12 @@ int kuhn(const std::vector<std::vector<int>> &adj, std::vector<int> &matching, s
     {
         isFull = false;
         used = std::vector<bool>(adj.size(), false);
-        for (int v : part1)
+        for (auto [w, v] : part1)
             if (matching[v] == -1 && dfs(dfs, v))
                 isFull = true;
     }
     int counter = 0;
-    for (int v : part1)
+    for (auto [w, v] : part1)
         counter += (matching[v] != -1);
     return counter;
 }
