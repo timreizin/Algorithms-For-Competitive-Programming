a716fdd8c4fe9d5e13191fb5584045d1d797ae54
Tymofii Reizin <46033117+timreizin@users.noreply.github.com>
2023-09-19 22:19:42 +0200

    Update DynamicSegmentTree.cpp

--- Data Structures/DynamicSegmentTree.cpp
+++ Data Structures/DynamicSegmentTree.cpp
@@ -1,29 +1,33 @@
+template <class T>
 class DynamicSegmentTree
 {
 private:
-    
+
     int index;
-    ll minValue, maxValue;
-    vector<ll> tree;
-    vector<pair<int, int>> child;
-    
-    ll get(ll a, ll b, ll l, ll r, int v)
+    long long minValue, maxValue;
+    std::vector<T> tree;
+    std::vector<std::pair<int, int>> child;
+
+    T get(long long a, long long b, long l, long r, int v)
     {
-        if (v == -1 || a > r || b < l) return 0ll;
-        if (a <= l && b >= r) return tree[v];
-        ll m = l + (r - l) / 2;
-        return get(a, min(b, m), l, m, child[v].first) + get(max(a, m + 1), b, m + 1, r, child[v].second);
+        if (v == -1 || a > r || b < l)
+            return T(0);
+        if (a <= l && b >= r)
+            return tree[v];
+        long long m = l + (r - l) / 2;
+        return get(a, std::min(b, m), l, m, child[v].first) + get(std::max(a, m + 1), b, m + 1, r, child[v].second);
     }
-    
-    void update(ll pos, ll l, ll r, int v, ll value)
+
+    void update(long long pos, long long l, long long r, int v, T value)
     {
-        if (l > r) return;
+        if (l > r)
+            return;
         if (l == r)
         {
             tree[v] += value;
             return;
         }
-        ll m = l + (r - l) / 2;
+        long long m = l + (r - l) / 2;
         if (pos <= m)
         {
             if (child[v].first == -1)
@@ -46,27 +50,27 @@ private:
         }
         tree[v] = (child[v].first == -1 ? 0 : tree[child[v].first]) + (child[v].second == -1 ? 0 : tree[child[v].second]);
     }
-    
+
 public:
-    
-    DynamicSegmentTree(ll maxVal) : index(1), minValue(0), maxValue(maxVal)
+
+    DynamicSegmentTree(long long maxVal) : index(1), minValue(0), maxValue(maxVal)
     {
         tree.push_back(0);
         child.push_back({-1, -1});
     }
-    
-    DynamicSegmentTree(ll minVal, ll maxVal) : index(1), minValue(minVal), maxValue(maxVal)
+
+    DynamicSegmentTree(long long minVal, long long maxVal) : index(1), minValue(minVal), maxValue(maxVal)
     {
         tree.push_back(0);
         child.push_back({-1, -1});
     }
-    
-    ll get(ll a, ll b)
+
+    T get(long long a, long long b)
     {
         return get(a, b, minValue, maxValue, 0);
     }
-    
-    void update(ll pos, ll value)
+
+    void update(long long pos, long long value)
     {
         update(pos, minValue, maxValue, 0, value);
     }
