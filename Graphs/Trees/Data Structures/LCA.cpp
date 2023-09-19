16fdbafd77d9c43c3ba3aa35faa0ad1134c276ff
Tymofii Reizin <46033117+timreizin@users.noreply.github.com>
2023-09-19 22:09:31 +0200

    Update LCA.cpp

--- Graphs/Trees/Data Structures/LCA.cpp
+++ Graphs/Trees/Data Structures/LCA.cpp
@@ -2,17 +2,20 @@ class LCA
 {
 private:
 
-    vector<int> tin;
-    vector<int> tout;
-    vector<vector<int>> up;
+    std::vector<int> tin;
+    std::vector<int> tout;
+    std::vector<std::vector<int>> up;
     int log2;
 
-    void dfs(int v, int p, int& timer, vector<vector<int>>& adj)
+    void dfs(int v, int p, int &timer, std::vector<std::vector<int>> &adj)
     {
         tin[v] = timer++;
         up[v][0] = p;
-        for (int i = 1; i <= log2; ++i) up[v][i] = up[up[v][i - 1]][i - 1];
-        for (int u : adj[v]) if (u != p) dfs(u, v, timer, adj);
+        for (int i = 1; i <= log2; ++i)
+            up[v][i] = up[up[v][i - 1]][i - 1];
+        for (int u : adj[v])
+            if (u != p)
+                dfs(u, v, timer, adj);
         tout[v] = timer - 1;
     }
 
@@ -23,7 +26,7 @@ private:
 
 public:
 
-    LCA(vector<vector<int>>& adj, vector<int> roots = vector<int>{1}) : log2(0)
+    LCA(std::vector<std::vector<int>> &adj, std::vector<int> roots = std::vector<int>{1}) : log2(0)
     {
         tin.resize(adj.size());
         tout.resize(adj.size());
@@ -33,17 +36,23 @@ public:
             n = (n + 1) / 2;
             ++log2;
         }
-        up = vector<vector<int>>(adj.size(), vector<int>(log2 + 1));
+        up = std::vector<std::vector<int>>(adj.size(), std::vector<int>(log2 + 1));
         int timer = 0;
-        for (int i : roots) dfs(i, i, timer, adj);
+        for (int i : roots)
+            dfs(i, i, timer, adj);
     }
 
     int get(int v, int u)
     {
-        if (up[v].back() != up[u].back()) return -1;
-        if (isAncestor(v, u)) return v;
-        if (isAncestor(u, v)) return u;
-        for (int i = log2; i >= 0; --i) if (!isAncestor(up[v][i], u)) v = up[v][i];
+        if (up[v].back() != up[u].back())
+            return -1;
+        if (isAncestor(v, u))
+            return v;
+        if (isAncestor(u, v))
+            return u;
+        for (int i = log2; i >= 0; --i)
+            if (!isAncestor(up[v][i], u))
+                v = up[v][i];
         return up[v][0];
     }
 
