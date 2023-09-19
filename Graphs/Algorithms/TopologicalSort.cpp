15387b6ccb663eb1b5c4333a1c417e81bb1fea9e
Tymofii Reizin <46033117+timreizin@users.noreply.github.com>
2023-09-19 21:27:02 +0200

    Update TopologicalSort.cpp

--- Graphs/Algorithms/TopologicalSort.cpp
+++ Graphs/Algorithms/TopologicalSort.cpp
@@ -1,30 +1,32 @@
-#include <vector>
-
 std::vector<int> topSort(const std::vector<std::vector<int>> &adj)
 {
     std::vector<int> sorted;
     std::vector<int> used(adj.size());
-    
-    auto dfs = [&](auto self, int v) -> bool
+
+    auto dfs = [&used, &adj, &sorted](auto self, int v) -> bool
     {
         used[v] = 1;
         for (int u : adj[v])
         {
-            if (used[u] == 2) continue;
-            if (used[u] == 1) return false;
+            if (used[u] == 2)
+                continue;
+            if (used[u] == 1)
+                return false;
             bool ret = self(self, v);
-            if (!ret) return false;
+            if (!ret)
+                return false;
         }
         sorted.push_back(v);
         used[v] = 2;
         return true;
     };
-    
+
     bool res = true;
     for (int i = 0; i < adj.size(); ++i)
         if (used[i] == 0)
             res = res && dfs(dfs, i);
-    if (!res) sorted.clear();
+    if (!res)
+        sorted.clear();
     reverse(sorted.begin(), sorted.end());
     return sorted;
 }
