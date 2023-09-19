608e6823b0d9029802da2ea88bf63d95eda70b46
Tymofii Reizin <46033117+timreizin@users.noreply.github.com>
2023-09-19 22:30:27 +0200

    Update LazyDynamicLiChaoTree.cpp

--- Data Structures/LazyDynamicLiChaoTree.cpp
+++ Data Structures/LazyDynamicLiChaoTree.cpp
@@ -2,14 +2,14 @@ template <class Function>
 class LazyDynamicLiChao
 {
 private:
-    
+
     using T = typename Function::type;
-    
+
     T minValue, maxValue;
-    vector<Function> tree;
-    vector<Function> mod;
-    vector<pair<int, int>> child;
-    
+    std::vector<Function> tree;
+    std::vector<Function> mod;
+    std::vector<std::pair<int, int>> child;
+
     void push(int v)
     {
         if (mod[v].k != 0)
@@ -41,15 +41,18 @@ private:
             mod[v].b = 0;
         }
     }
-    
+
     void update(T a, T b, T l, T r, int v, Function func)
     {
-        if (a > r || b < l) return;
+        if (a > r || b < l)
+            return;
         T m = l + (r - l) / 2;
         if (a == l && b == r)
         {
-            if (func(m) > tree[v](m)) swap(tree[v], func);
-            if (func(l) <= tree[v](l) && func(r) <= tree[v](r)) return;
+            if (func(m) > tree[v](m))
+                swap(tree[v], func);
+            if (func(l) <= tree[v](l) && func(r) <= tree[v](r))
+                return;
             push(v);
             if (func(l) > tree[v](l))
             {
@@ -99,20 +102,25 @@ private:
             update(max(a, m + 1), b, m + 1, r, child[v].second, func);
         }
     }
-    
+
     T get(T p, T l, T r, int v)
     {
-        if (v == -1) return 0;
-        if (l == r) return tree[v](p);
+        if (v == -1)
+            return 0;
+        if (l == r)
+            return tree[v](p);
         T m = l + (r - l) / 2;
         push(v);
-        if (p <= m) return max(tree[v](p), get(p, l, m, child[v].first));
-        else return max(tree[v](p), get(p, m + 1, r, child[v].second));
+        if (p <= m)
+            return max(tree[v](p), get(p, l, m, child[v].first));
+        else
+            return max(tree[v](p), get(p, m + 1, r, child[v].second));
     }
-    
+
     void addK(T a, T b, T k, T l, T r, int v)
     {
-        if (a > r || b < l) return;
+        if (a > r || b < l)
+            return;
         if (a == l && b == r)
         {
             mod[v].k += k;
@@ -144,10 +152,11 @@ private:
             addK(max(a, m + 1), b, k, m + 1, r, child[v].second);
         }
     }
-    
+
     void addB(T a, T b, T k, T l, T r, int v)
     {
-        if (a > r || b < l) return;
+        if (a > r || b < l)
+            return;
         if (a == l && b == r)
         {
             mod[v].b += k;
@@ -179,56 +188,56 @@ private:
             addB(max(a, m + 1), b, k, m + 1, r, child[v].second);
         }
     }
-    
+
 public:
-    
+
     LazyDynamicLiChao(T maxValue) : minValue(0), maxValue(maxValue)
     {
         child.emplace_back(-1, -1);
         tree.push_back(Line<T>());
         mod.push_back(Line<T>());
     }
-    
+
     LazyDynamicLiChao(T minValue = -1e9, T maxValue = 1e9) : minValue(minValue), maxValue(maxValue)
     {
         child.emplace_back(-1, -1);
         tree.push_back(Line<T>());
         mod.push_back(Line<T>());
     }
-    
+
     void addLine(Function func)
     {
         update(minValue, maxValue, minValue, maxValue, 0, func);
     }
-    
+
     void addLine(T a, T b, Function func)
     {
         update(a, b, minValue, maxValue, 0, func);
     }
-    
+
     void addK(T a, T b, T k)
     {
         addK(a, b, k, minValue, maxValue, 0);
     }
-    
+
     void addK(T k)
     {
         addK(minValue, maxValue, k, minValue, maxValue, 0);
     }
-    
+
     void addB(T a, T b, T k)
     {
         addB(a, b, k, minValue, maxValue, 0);
     }
-    
+
     void addB(T k)
     {
         addB(minValue, maxValue, k, minValue, maxValue, 0);
     }
-    
+
     T get(T p)
     {
         return get(p, minValue, maxValue, 0);
     }
-    
+
 };
